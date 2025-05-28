f9dacbc0bf528aa6be24f8400375f970038dd5d2
ing-chiba <chiba@in-g.jp>
2025-05-29 00:57:38 +0900

    更新

--- src/server/api/routers/admin/features/approveCompany.ts
+++ src/server/api/routers/admin/features/approveCompany.ts
@@ -1,12 +1,35 @@
+import { createSupabaseAdminClient } from "@/lib/supabase/serverClient";
 import { publicProcedure } from "@/server/api/trpc";
 import { approveCompanySchema } from "@/validations/admin/approveCompany";
+import { TRPCError } from "@trpc/server";
 
 export const approveCompany = publicProcedure
   .input(approveCompanySchema)
   .mutation(async ({ ctx, input }) => {
+    // 会社情報取得
     const company = await ctx.db.company.update({
       where: { id: input.companyId },
-      data: { isApproved: true },
+      data: { isApproved: false },
+      include: { business: true },
     });
+
+    // businessがなければエラー
+    if (!company.business) {
+      throw new TRPCError({
+        code: "BAD_REQUEST",
+        message: "ビジネス情報がありません。",
+      });
+    }
+
+    // Supabaseユーザー発行
+    const supabase = await createSupabaseAdminClient();
+    await supabase.auth.admin.inviteUserByEmail("test@test.com", {
+      data: {
+        role: "company",
+        displayName: company.business.contactName,
+      },
+      redirectTo: `${process.env.NEXT_PUBLIC_APP_URL}/auth/callback`,
+    });
+
     return company;
   });
