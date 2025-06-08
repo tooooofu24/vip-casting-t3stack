6f825040e53d0969e657635482dd4b094ac751a8
ing-chiba <chiba@in-g.jp>
2025-06-08 21:46:01 +0900

    実装

--- src/server/api/routers/admin/features/auth/router.ts
+++ src/server/api/routers/admin/features/auth/router.ts
@@ -1,8 +1,6 @@
 import { login } from "@/server/api/routers/admin/features/auth/login/api";
-import { signUp } from "@/server/api/routers/admin/features/auth/signUp/api";
 import { createTRPCRouter } from "@/server/api/trpc";
 
 export const authRouter = createTRPCRouter({
   login,
-  signUp,
 });
--- src/server/api/routers/admin/features/auth/signUp/validation.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "@/lib/zod";
-
-export const adminSignUpSchema = z.object({
-  email: z.string().min(1).email(),
-  password: z.string().min(8),
-  displayName: z.string().min(1),
-});
-
-export type AdminSignUpRequest = z.infer<typeof adminSignUpSchema>;
