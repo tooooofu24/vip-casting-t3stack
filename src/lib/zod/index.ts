61b98de350a4f0ba41143d54857612caf64d3e50
ing-chiba <chiba@in-g.jp>
2025-07-29 03:13:08 +0900

    zodの対応

--- src/lib/zod/index.ts
+++ src/lib/zod/index.ts
@@ -1,6 +1,11 @@
 import { z } from "zod"; // eslint-disable-line no-restricted-imports
+
+import { localeError } from "@/lib/zod/error";
 import { ja } from "zod/locales";
 
-z.config(ja());
+z.config({
+  ...ja(),
+  localeError: localeError(),
+});
 
 export { z };
